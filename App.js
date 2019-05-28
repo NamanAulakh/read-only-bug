edac38350703de29eb32f8ef4962530b6a8fe90b
Naman Aulakh <aulakh.naman@gmail.com>
2019-05-28 17:00:55 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Comp from './Comp';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text]);
+    default:
+      return state;
+  }
+}
+
+const store = createStore(todos, ['Use Redux']);
+
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   text: 'Read the docs'
+// })
+
+console.log(store.getState(), '............');
+// [ 'Use Redux', 'Read the docs' ]
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Provider store={store}>
+        <Comp />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
